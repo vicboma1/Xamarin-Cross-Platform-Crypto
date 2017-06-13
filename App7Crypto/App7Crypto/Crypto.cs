7fb4846df7c1167a22242f6d36815e9eb0e3e675
Victor Bolinches <vicboma1@users.noreply.github.com>
2017-06-13 22:02:39 +0200

    Add files via upload

---
+++ App7Crypto/App7Crypto/Crypto.cs
@@ -0,0 +1,38 @@
+﻿using PCLCrypto;
+using static PCLCrypto.WinRTCrypto;
+using System.Text;
+using System;
+using System.IO;
+
+namespace Crypto
+{
+    public static class Crypto
+    {
+
+        //Esto debe de ser un valor accesible encriptado con una xor
+        private static string key = "&%$€@Ññ";
+
+        private static byte[] GetBytesPassword(string str, int bufferInBytes = 16, int keyLengthInBytes = 32, int iterations = 1000)
+        {
+            var buffer = WinRTCrypto.CryptographicBuffer.GenerateRandom(bufferInBytes);
+            return NetFxCrypto.DeriveBytes.GetBytes(str, buffer, iterations, keyLengthInBytes);
+        }
+
+        private static string ToCryto(this string data)
+        {
+            var result = new StringBuilder();
+
+            for (int c = 0; c < data.Length; c++)
+                result.Append((char)((uint)data[c] ^ (uint)key[c % key.Length]));
+
+            return result.ToString();
+        }
+
+        public static string ToStringEncrypt(this string data) => data.ToCryto();
+        
+        public static byte [] ToByteEncrypt(this string data) => Encoding.UTF8.GetBytes(data.ToCryto());
+
+        public static string ToByteEncrypt(this byte[] data) => Encoding.UTF8.GetString(data,0 ,data.Length).ToStringEncrypt();
+
+    }
+}
---
+++ App7Crypto/App7Crypto/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Crypto;
+
+namespace App7Crypto
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Clicked(object sender, EventArgs e)
+        {
+            var strEncrypt = this.entry.Text.ToStringEncrypt();
+            var strbEncrypt = this.entry.Text.ToByteEncrypt();
+      
+            this.entryBinaryEncrypt.Text = Convert.ToBase64String(strbEncrypt);
+            this.entryStringEncrypt.Text = strEncrypt;
+
+            var strDecrypt = strEncrypt.ToStringEncrypt();
+            var strbDecrypt = strbEncrypt.ToByteEncrypt();
+
+            this.entryStringDecrypt.Text= Convert.ToString(strDecrypt);
+            this.entryBinaryDecrypt.Text = strbDecrypt;
+
+        }
+    }
+}
